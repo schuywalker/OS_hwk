3cea9df0f07737990ebe631d2ff6f16800586aec
schuyler <sasplin@ewu.edu>
2022-10-24 18:32:33 -0700

    memory-user works

--- ch13_VirtualMemoryIntro/memory-user.c
+++ ch13_VirtualMemoryIntro/memory-user.c
@@ -2,47 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+;
+
 int main(int argc, char *argv[]) {
 
+    int infinite = 0; // false
+
     if (argc < 2) {
-        printf("please rerun with:\nfirst argument as bytes to be allocated and\nthe second arg as the number of times to loop through the allocated memory.\n");
+        printf("please rerun with:\nfirst argument as bytes to be allocated and\nthe second arg is optional, and functions as the number of times to loop through the allocated memory.\n");
         exit(-1);
+    } else if (argc == 2) {
+        printf("ctrl to stop program\n");
+        infinite = 1; // true
     }
 
-    int number_of_bytes = atoi(argv[0]);
-    int loops = atoi(argv[1]);
-
-    fprintf("%d",loops);
-    exit(0);
-
-
-
-    fprintf("%d   %d\n",number_of_bytes,loops);
-    
+    int number_of_bytes = atoi(argv[1]);
     char *bytes = malloc(sizeof(char) * (number_of_bytes));
-
-    int i = 0;
-    // int j = 0;
     
-while (i < loops) {
-    for (int j = 0; j < number_of_bytes; j++) {
-            if (i == 0) {
-                bytes[i] = 'a';
-                fprintf("%c  ",bytes[i]);
-            }
-            else {
-                bytes[i] = 'b';
-                fprintf("%c  ",bytes[i]);
-            }
+    if (infinite) {
+        printf("number of bytes: %d  loops: infinite\n",number_of_bytes);
+        int i = 0;
+        while (1) {
+        for (int j = 0; j < number_of_bytes; j++) {
+            bytes[i] = ((i+65)%122); // A = 65, z = 122
+            printf("%c  ",bytes[i]);
         }
-        printf("\n");
         i++;
-}
-    // for (int i = 0; i < loops; i++) {
-        
-    // }
-
-    printf("%d",bytes);
-
+        printf("\n");
+    }
+    } else {    
+        int loops = atoi(argv[2]); 
+        printf("number of bytes: %d  loops: %d\n",number_of_bytes,loops);
+        int i = 0;  
+        while (i < loops) {
+            for (int j = 0; j < number_of_bytes; j++) {
+            bytes[i] = ((i+65)%122); // A = 65, z = 122
+            printf("%c  ",bytes[i]);
+                printf("%c  ",bytes[i]);
+            
+            }
+            printf("\n");
+            i++;
+        }   
+    }
     return 0;
 }
